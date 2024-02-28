992399c2ea001a9d1c21f6a98984f777d38d5f4d
yactouat <yactouat@hotmail.com>
2024-02-28 11:30:45 +0100

    triggering web search if does not know the answer

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import os
 # print(os.getenv('OPENAI_API_KEY'))
 
 from langchain_openai import ChatOpenAI
+# this import allows us to format prompts with variables
 from langchain.prompts import PromptTemplate
-
 
 # we create an instance of Chat-GPT programmatically
 chat_gpt = ChatOpenAI()
@@ -16,10 +16,27 @@ control_prompt_template = """Do you have the answer to the question below, delim
 {question}
 -----------------------------------
 If the answer is yes, give us your source."""
-initial_prompt = "What is LangChain and what can I do with it?"
 
-prompt = PromptTemplate.from_template(control_prompt_template)
-control_prompt = prompt.format(question=initial_prompt)
+formatting_prompt_template = """You are given an answer in natural language, delimited by dashes below.
+-----------------------------------
+{answer}
+-----------------------------------
+Your job is to output a boolean from the answer:
+- true, if the answer shows that the it knows the answer to the question, false otherwise."""
 
-res = chat_gpt.invoke(control_prompt)
-print(res)
+# initial_prompt = "What is LangChain and what can I do with it?"
+initial_prompt = "What is the capital of France?"
+
+# we check if the bot knows the answer to the question
+knowledge_base_prompt = PromptTemplate.from_template(control_prompt_template)
+control_prompt = knowledge_base_prompt.format(question=initial_prompt)
+knowledge_base_answer = chat_gpt.invoke(control_prompt)
+
+# we format the answer in a JSON object
+formatting_prompt = PromptTemplate.from_template(formatting_prompt_template)
+formatting_response = chat_gpt.invoke(formatting_prompt.format(answer=knowledge_base_answer))
+
+if (bool(formatting_response.content)):
+    print(knowledge_base_answer)
+else:
+    print("TODO make a web search to find the answer to the question.")
